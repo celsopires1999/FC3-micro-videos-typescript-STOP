e419c0a261de58f0bc765e2b743f5ee1340b3f16
celsopires1999 <celso.pires1999@gmail.com>
2022-04-06 04:02:55 +0000

    feat: :sparkles: Teste InMemoryRepository

---
+++ src/@seedwork/domain/repository/in-memory-repository.spec.ts
@@ -0,0 +1,107 @@
+import Entity from "../entity/entity";
+import NotFoundError from "../errors/not-found.error";
+import InMemoryRepository from "./in-memory-repository";
+
+type StubEntityProps = {
+  name: string;
+  price: number;
+};
+
+class StubEntity extends Entity<StubEntityProps> {}
+class StubInMemoryRepository extends InMemoryRepository<StubEntity> {}
+
+describe("InMemoryRepository Unit Tests", () => {
+  let repository: StubInMemoryRepository;
+  beforeEach(() => (repository = new StubInMemoryRepository()));
+
+  it("should insert a new entity", async () => {
+    const entity = new StubEntity({ name: "some name", price: 10 });
+    await repository.insert(entity);
+    expect(entity.toJSON()).toStrictEqual(repository.items[0].toJSON());
+  });
+
+  it("should throw an error when entity has already been included", async () => {
+    const entity = new StubEntity({ name: "some name", price: 10 });
+    await repository.insert(entity);
+    expect(repository.insert(entity)).rejects.toThrowError(
+      `Entity has already been included with ID ${entity.id}`
+    );
+  });
+
+  it("should throw an error when entity has not been found", async () => {
+    expect(repository.findById("fake id")).rejects.toThrow(
+      new NotFoundError("Entity not found using ID fake id")
+    );
+    expect(
+      repository.findById("312cffad-1938-489e-a706-643dc9a3cfd3")
+    ).rejects.toThrow(
+      new NotFoundError(
+        "Entity not found using ID 312cffad-1938-489e-a706-643dc9a3cfd3"
+      )
+    );
+  });
+
+  it("should find an entity by Id", async () => {
+    const entity = new StubEntity({ name: "some name", price: 10 });
+    await repository.insert(entity);
+
+    let entityFound = await repository.findById(entity.id);
+    expect(entity.toJSON()).toStrictEqual(entityFound.toJSON());
+
+    entityFound = await repository.findById(entity.uniqueEntityId);
+    expect(entity.toJSON()).toStrictEqual(entityFound.toJSON());
+  });
+
+  it("should return all entities", async () => {
+    const entity = new StubEntity({ name: "some name", price: 10 });
+    await repository.insert(entity);
+
+    const entities = await repository.findAll();
+    expect(entities).toStrictEqual([entity]);
+  });
+
+  it("should throw an error on update when entity has not been found", async () => {
+    const entity = new StubEntity({ name: "some name", price: 10 });
+    expect(repository.update(entity)).rejects.toThrow(
+      new NotFoundError(`Entity not found using ID ${entity.id}`)
+    );
+  });
+
+  it("should update an entity", async () => {
+    const entity = new StubEntity({ name: "some name", price: 10 });
+    await repository.insert(entity);
+
+    const entityUpdated = new StubEntity(
+      { name: "new name", price: 20 },
+      entity.uniqueEntityId
+    );
+    await repository.update(entityUpdated);
+
+    expect(entityUpdated.toJSON()).toStrictEqual(repository.items[0].toJSON());
+  });
+
+  it("should throw an error on delete when entity has not been found", async () => {
+    expect(repository.delete("fake id")).rejects.toThrow(
+      new NotFoundError("Entity not found using ID fake id")
+    );
+    expect(
+      repository.delete("312cffad-1938-489e-a706-643dc9a3cfd3")
+    ).rejects.toThrow(
+      new NotFoundError(
+        "Entity not found using ID 312cffad-1938-489e-a706-643dc9a3cfd3"
+      )
+    );
+  });
+
+  it("should delete an entity", async () => {
+    const entity = new StubEntity({ name: "some name", price: 10 });
+    await repository.insert(entity);
+
+    await repository.delete(entity.id);
+    expect(repository.items).toHaveLength(0);
+
+    await repository.insert(entity);
+    await repository.delete(entity.uniqueEntityId);
+    expect(repository.items).toHaveLength(0);
+  });
+});
---
+++ src/@seedwork/domain/repository/in-memory-repository.ts
@@ -0,0 +1,60 @@
+import Entity from "../entity/entity";
+import NotFoundError from "../errors/not-found.error";
+import uniqueEntityId from "../value-objects/unique-entity-id.vo";
+import { RepositoryInterface } from "./repository-contracts";
+
+export default abstract class InMemoryRepository<E extends Entity>
+  implements RepositoryInterface<E>
+{
+  items: E[] = [];
+
+  async insert(entity: E): Promise<void> {
+    let item: E;
+
+    try {
+      item = await this._get(entity.id);
+    } catch (e) {}
+
+    if (item) {
+      throw new Error(`Entity has already been included with ID ${entity.id}`);
+    }
+
+    this.items.push(entity);
+  }
+
+  async findById(id: string | uniqueEntityId): Promise<E> {
+    let _id = `${id}`;
+    if (typeof id !== "string") {
+      _id = id.value;
+    }
+    return this._get(_id);
+  }
+
+  async findAll(): Promise<E[]> {
+    return this.items;
+  }
+
+  async update(entity: E): Promise<void> {
+    await this._get(entity.id);
+    const indexFound = this.items.findIndex((i) => i.id === entity.id);
+    this.items[indexFound] = entity;
+  }
+
+  async delete(id: string | uniqueEntityId): Promise<void> {
+    let _id = `${id}`;
+    if (typeof id !== "string") {
+      _id = id.value;
+    }
+    await this._get(_id);
+    const indexFound = this.items.findIndex((i) => i.id === _id);
+    this.items.splice(indexFound, 1);
+  }
+
+  protected async _get(_id: string): Promise<E> {
+    const item = this.items.find((i) => i.id === _id);
+    if (!item) {
+      throw new NotFoundError(`Entity not found using ID ${_id}`);
+    }
+    return item;
+  }
+}
