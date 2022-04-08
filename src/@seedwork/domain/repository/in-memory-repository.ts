82122b06ac9badba940ad6a80959128533d0050d
celsopires1999 <celso.pires1999@gmail.com>
2022-04-08 22:52:09 +0000

    feat: :sparkles: Criando lógica do método de busca personalizado do repositório em memória

--- src/@seedwork/domain/repository/in-memory-repository.ts
+++ src/@seedwork/domain/repository/in-memory-repository.ts
@@ -3,6 +3,8 @@ import NotFoundError from "../errors/not-found.error";
 import uniqueEntityId from "../value-objects/unique-entity-id.vo";
 import {
   RepositoryInterface,
+  SearchParams,
+  SearchResult,
   SerachableRepositoryInterface,
 } from "./repository-contracts";
 
@@ -64,9 +66,48 @@ export abstract class InMemoryRepository<E extends Entity>
 
 export abstract class InMemorySearchableRepository<E extends Entity>
   extends InMemoryRepository<E>
-  implements SerachableRepositoryInterface<E, any, any>
+  implements SerachableRepositoryInterface<E>
 {
-  search(props: any): Promise<any> {
-    throw new Error("Method not implemented.");
+  async search(props: SearchParams): Promise<SearchResult<E>> {
+    const itemsFiltered = await this.applyFilter(this.items, props.filter);
+
+    const itemsSorted = await this.applySort(
+      itemsFiltered,
+      props.sort,
+      props.sort_dir
+    );
+
+    const itemsPaginated = await this.applyPaginate(
+      itemsSorted,
+      props.page,
+      props.per_page
+    );
+
+    return new SearchResult({
+      items: itemsPaginated,
+      total: itemsFiltered.length,
+      current_page: props.page,
+      per_page: props.per_page,
+      sort: props.sort,
+      sort_dir: props.sort_dir,
+      filter: props.filter,
+    });
   }
+
+  protected abstract async applyFilter(
+    items: E[],
+    filter: string | null
+  ): Promise<E[]>;
+
+  protected abstract async applySort(
+    items: E[],
+    sort: string | null,
+    sort_dir: string | null
+  ): Promise<E[]>;
+
+  protected abstract async applyPaginate(
+    items: E[],
+    page: SearchParams["page"],
+    per_page: SearchParams["per_page"]
+  ): Promise<E[]>;
 }
