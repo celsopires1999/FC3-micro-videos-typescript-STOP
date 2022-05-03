05a72a0c6f565d8d793a6a48f8587531b492f997
celsopires1999 <celso.pires1999@gmail.com>
2022-05-03 15:45:04 +0000

    feat: Usando SWC com Nest.js

--- src/nestjs/jest.config.ts
+++ src/nestjs/jest.config.ts
@@ -7,7 +7,7 @@ export default {
   rootDir: 'src',
   testRegex: '.*\\.spec\\.ts$',
   transform: {
-    '^.+\\.(t|j)s$': 'ts-jest',
+    '^.+\\.(t|j)s$': '@swc/jest',
   },
   collectCoverageFrom: ['**/*.(t|j)s'],
   coverageDirectory: '../coverage',
