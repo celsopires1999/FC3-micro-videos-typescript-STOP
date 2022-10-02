91df5d066addce11e6be3976fade683fbc8a64e6
celsopires1999 <celso.pires1999@gmail.com>
2022-10-02 22:44:26 +0000

    feat: Criando presenter para serializar busca de categorias

--- src/nestjs/src/categories/presenter/category.presenter.ts
+++ src/nestjs/src/categories/presenter/category.presenter.ts
@@ -1,5 +1,8 @@
-import { CategoryOutput } from '@fc/micro-videos/category/application';
-import { Transform } from 'class-transformer';
+import {
+  CategoryOutput,
+  ListCategoriesUseCase,
+} from '@fc/micro-videos/category/application';
+import { Transform, Exclude, Expose } from 'class-transformer';
 
 export class CategoryPresenter {
   id: string;
@@ -16,4 +19,63 @@ export class CategoryPresenter {
     this.is_active = output.is_active;
     this.created_at = output.created_at;
   }
+}
+
+export type PaginationPresenterProps = {
+  current_page: number;
+  per_page: number;
+  last_page: number;
+  total: number;
+};
+
+export class PaginationPresenter {
+  @Transform(({ value }) => parseInt(value))
+  current_page: number;
+  @Transform(({ value }) => parseInt(value))
+  per_page: number;
+  @Transform(({ value }) => parseInt(value))
+  last_page: number;
+  @Transform(({ value }) => parseInt(value))
+  total: number;
+
+  constructor(props: PaginationPresenterProps) {
+    this.current_page = props.current_page;
+    this.per_page = props.per_page;
+    this.last_page = props.last_page;
+    this.total = props.total;
+  }
+}
+
+export abstract class CollectionPresenter {
+  @Exclude()
+  protected paginationPresenter: PaginationPresenter;
+
+  constructor(props: PaginationPresenterProps) {
+    this.paginationPresenter = new PaginationPresenter(props);
+  }
+
+  @Expose({ name: 'meta' })
+  get meta() {
+    return this.paginationPresenter;
+  }
+
+  abstract get data();
+}
+
+export class CategoryCollectionPresenter extends CollectionPresenter {
+  // @Exclude()
+  // protected _data: CategoryPresenter[];
+  data: CategoryPresenter[];
+
+  constructor(output: ListCategoriesUseCase.Output) {
+    const { items, ...paginationProps } = output;
+    super(paginationProps);
+    // this._data = items.map((item) => new CategoryPresenter(item));
+    this.data = items.map((item) => new CategoryPresenter(item));
+  }
+
+  // @Expose({ name: 'data' })
+  // get data() {
+  //   return this._data;
+  // }
 }
