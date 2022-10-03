93ad73c92e187deea59f73a28b3dc799b5eae7c7
celsopires1999 <celso.pires1999@gmail.com>
2022-10-03 03:10:18 +0000

    refactor: Criando módulo share no Nestjs para organizar artefatos comuns

--- src/nestjs/src/categories/presenter/category.presenter.ts
+++ src/nestjs/src/categories/presenter/category.presenter.ts
@@ -2,7 +2,8 @@ import {
   CategoryOutput,
   ListCategoriesUseCase,
 } from '@fc/micro-videos/category/application';
-import { Exclude, Expose, Transform } from 'class-transformer';
+import { Transform } from 'class-transformer';
+import { CollectionPresenter } from '../../@share/presenters/collection.presenter';
 
 export class CategoryPresenter {
   id: string;
@@ -20,48 +21,6 @@ export class CategoryPresenter {
     this.created_at = output.created_at;
   }
 }
-
-export type PaginationPresenterProps = {
-  current_page: number;
-  per_page: number;
-  last_page: number;
-  total: number;
-};
-
-export class PaginationPresenter {
-  @Transform(({ value }) => parseInt(value))
-  current_page: number;
-  @Transform(({ value }) => parseInt(value))
-  per_page: number;
-  @Transform(({ value }) => parseInt(value))
-  last_page: number;
-  @Transform(({ value }) => parseInt(value))
-  total: number;
-
-  constructor(props: PaginationPresenterProps) {
-    this.current_page = props.current_page;
-    this.per_page = props.per_page;
-    this.last_page = props.last_page;
-    this.total = props.total;
-  }
-}
-
-export abstract class CollectionPresenter {
-  @Exclude()
-  protected paginationPresenter: PaginationPresenter;
-
-  constructor(props: PaginationPresenterProps) {
-    this.paginationPresenter = new PaginationPresenter(props);
-  }
-
-  @Expose({ name: 'meta' })
-  get meta() {
-    return this.paginationPresenter;
-  }
-
-  abstract get data();
-}
-
 export class CategoryCollectionPresenter extends CollectionPresenter {
   data: CategoryPresenter[];
 
