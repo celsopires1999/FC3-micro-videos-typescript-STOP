12086cdf59a66aac42075f800032c01f0f5c63ba
celsopires1999 <celso.pires1999@gmail.com>
2022-10-04 15:32:18 +0000

    test: Primeiro contato com teste end to end e a lib supertest

--- src/nestjs/test/app.e2e-spec.ts
+++ src/nestjs/test/app.e2e-spec.ts
@@ -15,10 +15,21 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
+  it('/ (GET)', (done) => {
+    request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Hello World!')
+      .end((err, res) => {
+        if (err) {
+          console.log('err: ', err);
+          console.log('statusCode:', res.statusCode);
+          console.log('res.text: ', res.text);
+          console.log('body:', res.body);
+          console.log('headers: ', res.headers);
+          return done(err);
+        }
+        return done();
+      });
   });
 });
