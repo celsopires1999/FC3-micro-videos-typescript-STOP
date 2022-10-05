d92f9d77da1e35b7ed943069f3276f0e3351ad14
celsopires1999 <celso.pires1999@gmail.com>
2022-10-05 22:03:11 +0000

    test: Teste WrapperDataInterceptor

--- src/nestjs/src/@share/interceptors/wrapper-data.interceptor.spec.ts
+++ src/nestjs/src/@share/interceptors/wrapper-data.interceptor.spec.ts
@@ -1,7 +1,42 @@
+import { of, throwError } from 'rxjs';
 import { WrapperDataInterceptor } from './wrapper-data.interceptor';
 
-describe('WrapperDataInterceptor', () => {
-  it('should be defined', () => {
-    expect(new WrapperDataInterceptor()).toBeDefined();
+describe('WrapperDataInterceptor Unit Tests', () => {
+  let interceptor: WrapperDataInterceptor;
+
+  beforeEach(() => {
+    interceptor = new WrapperDataInterceptor();
+  });
+  it('should wrapper with data key', (done) => {
+    expect(interceptor).toBeDefined();
+    const obs$ = interceptor.intercept({} as any, {
+      handle: () => of({ name: 'test' }),
+    });
+    obs$
+      .subscribe({
+        next: (value) => {
+          expect(value).toStrictEqual({ data: { name: 'test' } });
+        },
+      })
+      .add(() => {
+        return done();
+      });
+  });
+
+  it('should not wrapper when meta key is present', (done) => {
+    expect(interceptor).toBeDefined();
+    const result = { data: [{ name: 'test' }], meta: { total: 1 } };
+    const obs$ = interceptor.intercept({} as any, {
+      handle: () => of(result),
+    });
+    obs$
+      .subscribe({
+        next: (value) => {
+          expect(value).toEqual({ ...result });
+        },
+      })
+      .add(() => {
+        return done();
+      });
   });
 });
