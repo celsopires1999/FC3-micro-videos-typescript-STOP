6299d293af02ced6dc69aa23b6eeb2e2363f80d3
celsopires1999 <celso.pires1999@gmail.com>
2022-10-06 01:17:36 +0000

    feat: Adicionando regras de validação no DTO de criação e atualização de categoria

--- src/nestjs/src/categories/dto/create-category.dto.ts
+++ src/nestjs/src/categories/dto/create-category.dto.ts
@@ -1,9 +1,16 @@
 import { CreateCategoryUseCase } from '@fc/micro-videos/category/application';
-import { IsNotEmpty } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class CreateCategoryDto implements CreateCategoryUseCase.Input {
+  @IsString()
   @IsNotEmpty()
   name: string;
+
+  @IsString()
+  @IsOptional()
   description?: string;
+
+  @IsString()
+  @IsOptional()
   is_active?: boolean;
 }
