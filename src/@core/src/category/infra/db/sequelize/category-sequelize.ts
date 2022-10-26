1f3f85c07cc9d4508ae3e3ac5413d36fe0f8e678
celsopires1999 <celso.pires1999@gmail.com>
2022-10-26 01:53:22 +0000

    test: Teste---busca de categoria com end to end

--- src/@core/src/category/infra/db/sequelize/category-sequelize.ts
+++ src/@core/src/category/infra/db/sequelize/category-sequelize.ts
@@ -17,8 +17,9 @@ import {
   UniqueEntityId,
   EntityValidationError,
   LoadEntityError,
+  SortDirection,
 } from "#seedwork/domain";
-import { Op } from "sequelize";
+import { literal, Op } from "sequelize";
 
 export namespace CategorySequelize {
   type CategoryModelProps = {
@@ -66,6 +67,11 @@ export namespace CategorySequelize {
     implements CategoryRepositoryContract.Repository
   {
     sortableFields: string[] = ["name", "created_at"];
+    orderBy = {
+      mysql: {
+        name: (sort_dir: SortDirection) => literal(`binary name ${sort_dir}`),
+      },
+    };
 
     constructor(private categoryModel: typeof CategoryModel) {}
 
@@ -86,7 +92,7 @@ export namespace CategorySequelize {
           where: { name: { [Op.like]: `%${props.filter}%` } },
         }),
         ...(props.sort && this.sortableFields.includes(props.sort)
-          ? { order: [[props.sort, props.sort_dir]] }
+          ? { order: this.formatSort(props.sort, props.sort_dir) }
           : { order: [["created_at", "DESC"]] }),
         offset,
         limit,
@@ -100,6 +106,14 @@ export namespace CategorySequelize {
         sort_dir: props.sort_dir,
         filter: props.filter,
       });
+    }
+
+    private formatSort(sort: string, sort_dir: SortDirection) {
+      const dialect = this.categoryModel.sequelize.getDialect();
+      if (this.orderBy[dialect] && this.orderBy[dialect][sort]) {
+        return this.orderBy[dialect][sort](sort_dir);
+      }
+      return [[sort, sort_dir]];
     }
 
     async insert(entity: Category): Promise<void> {
---
+++ src/nestjs/test/categories/list-category.e2e-spec.ts
@@ -0,0 +1,73 @@
+import { CategoryRepository } from '@fc/micro-videos/category/domain';
+import { getConnectionToken } from '@nestjs/sequelize';
+import { instanceToPlain } from 'class-transformer';
+import request from 'supertest';
+import { startApp } from '../../src/@share/testing/helpers';
+import { CATEGORY_PROVIDERS } from '../../src/categories/category.providers';
+import { ListCategoriesFixture } from '../../src/categories/fixtures';
+import { CategoryCollectionPresenter } from '../../src/categories/presenter/category.presenter';
+
+describe('CategoriesController (e2e)', () => {
+  describe('/categories (GET)', () => {
+    describe('should return categories ordered by created_at when query is empty', () => {
+      let categoryRepo: CategoryRepository.Repository;
+      const nestApp = startApp();
+      const { arrange, entitiesMap } =
+        ListCategoriesFixture.arrangeIncrementedWithCreatedAt();
+      const entities = Object.values(entitiesMap);
+
+      beforeEach(async () => {
+        categoryRepo = nestApp.app.get<CategoryRepository.Repository>(
+          CATEGORY_PROVIDERS.REPOSITORIES.CATEGORY_REPOSITORY.provide,
+        );
+        const sequelize = nestApp.app.get(getConnectionToken());
+        await sequelize.sync({ force: true });
+        await categoryRepo.bulkInsert(entities);
+      });
+
+      test.each(arrange)(
+        'with send data: $send_data',
+        async ({ send_data, expected }) => {
+          const queryParams = new URLSearchParams(send_data as any).toString();
+          const res = await request(nestApp.app.getHttpServer())
+            .get(`/categories?${queryParams}`)
+            .expect(200);
+          expect(Object.keys(res.body)).toStrictEqual(['data', 'meta']);
+          const presenter = new CategoryCollectionPresenter(expected);
+          const serialized = instanceToPlain(presenter);
+          expect(res.body).toEqual(serialized);
+        },
+      );
+    });
+
+    describe('should return categories using paginate, sort and filter', () => {
+      let categoryRepo: CategoryRepository.Repository;
+      const nestApp = startApp();
+      const { arrange, entitiesMap } = ListCategoriesFixture.arrangeUnsorted();
+      const entities = Object.values(entitiesMap);
+
+      beforeEach(async () => {
+        categoryRepo = nestApp.app.get<CategoryRepository.Repository>(
+          CATEGORY_PROVIDERS.REPOSITORIES.CATEGORY_REPOSITORY.provide,
+        );
+        const sequelize = nestApp.app.get(getConnectionToken());
+        await sequelize.sync({ force: true });
+        await categoryRepo.bulkInsert(entities);
+      });
+
+      test.each(arrange)(
+        'with send data: $send_data',
+        async ({ send_data, expected }) => {
+          const queryParams = new URLSearchParams(send_data as any).toString();
+          const res = await request(nestApp.app.getHttpServer())
+            .get(`/categories?${queryParams}`)
+            .expect(200);
+          expect(Object.keys(res.body)).toStrictEqual(['data', 'meta']);
+          const presenter = new CategoryCollectionPresenter(expected);
+          const serialized = instanceToPlain(presenter);
+          expect(res.body).toEqual(serialized);
+        },
+      );
+    });
+  });
+});
