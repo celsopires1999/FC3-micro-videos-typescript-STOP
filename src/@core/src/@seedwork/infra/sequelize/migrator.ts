796ca40c22ec1256b63ba36995b00219f0985ff3
celsopires1999 <celso.pires1999@gmail.com>
2022-11-12 21:59:18 +0000

    feat: Criando CLI no core para executar comandos do sistema de migração

--- src/@core/src/@seedwork/infra/sequelize/migrator.ts
+++ src/@core/src/@seedwork/infra/sequelize/migrator.ts
@@ -1,23 +1,20 @@
 import { join } from "path";
-import { Sequelize } from "sequelize-typescript";
-import { SequelizeStorage, Umzug } from "umzug";
+import { Sequelize } from "sequelize";
+import { SequelizeStorage, Umzug, UmzugOptions } from "umzug";
 
-const sequelize = new Sequelize({
-  dialect: "sqlite",
-  host: ":memory:",
-  logging: true,
-});
-
-const migrator = new Umzug({
-  migrations: {
-    glob: [
-      "*/infra/db/sequelize/migrations/*.{js,ts}",
-      { cwd: join(__dirname, "../../.."), ignore: ["*.d.ts"] },
-    ],
-  },
-  context: sequelize,
-  storage: new SequelizeStorage({ sequelize }),
-  logger: console,
-});
-
-migrator.runAsCLI();
+export const migrator = (
+  sequelize: Sequelize,
+  options?: Partial<UmzugOptions>
+) =>
+  new Umzug({
+    migrations: {
+      glob: [
+        "*/infra/db/sequelize/migrations/*.{js,ts}",
+        { cwd: join(__dirname, "../../.."), ignore: ["*.d.ts"] },
+      ],
+    },
+    context: sequelize,
+    storage: new SequelizeStorage({ sequelize }),
+    logger: console,
+    ...(options || {}),
+  });
