ad10740e061b888929931369237a305388c6ef58
celsopires1999 <celso.pires1999@gmail.com>
2022-11-13 19:41:28 +0000

    feat: Integrando Umzug no Nest.js

--- src/@core/src/@seedwork/infra/index.ts
+++ src/@core/src/@seedwork/infra/index.ts
@@ -1,4 +1,3 @@
 // created from 'create-ts-index'
 
-export * from './config';
 export * from './sequelize';
---
+++ src/@core/src/category/infra/db/sequelize/migrations/index.ts
@@ -0,0 +1,3 @@
+// created from 'create-ts-index'
+
+export * from './2022.11.10T23.57.12.create-categories-table';
