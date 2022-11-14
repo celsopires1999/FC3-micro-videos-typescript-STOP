be59e1721af0d2f345bdcc96ee390273527bce51
celsopires1999 <celso.pires1999@gmail.com>
2022-11-13 23:26:39 -0300

    ci: Ajustando configurações para ter todo ambiente 100 percent montado para desenvolver

--- .docker/start.sh
+++ .docker/start.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
+echo "####### Starting @core configs #######"
 if [ ! -f "./src/@core/.env.test" ]; then
     cp ./src/@core/.env.test.example ./src/@core/.env.test
 fi
 
+echo "####### Starting nestjs configs #######"
+if [ ! -f "./src/nestjs/envs/.env" ]; then
+    cp ./src/nestjs/envs/.env.example ./src/nestjs/envs/.env
+fi
+if [ ! -f "./src/nestjs/envs/.env.test" ]; then
+    cp ./src/nestjs/envs/.env.test.example ./src/nestjs/envs/.env.test
+fi
+if [ ! -f "./src/nestjs/envs/.env.e2e" ]; then
+    cp ./src/nestjs/envs/.env.e2e.example ./src/nestjs/envs/.env.e2e
+fi
+
 npm install
+
+echo "####### Building @core #######" 
+npm run build -w @fc/micro-videos
 
 tail -f /dev/null
 
