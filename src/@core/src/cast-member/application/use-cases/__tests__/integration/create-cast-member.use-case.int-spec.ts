2c6af94032c934632d06e31b03992ce0af312837
celsopires1999 <celso.pires1999@gmail.com>
2022-12-31 18:32:51 +0000

    feat: Overview - Caso de uso de atualizacao de Cast Member

--- src/@core/src/cast-member/application/use-cases/__tests__/integration/create-cast-member.use-case.int-spec.ts
+++ src/@core/src/cast-member/application/use-cases/__tests__/integration/create-cast-member.use-case.int-spec.ts
@@ -1,5 +1,7 @@
 import { CreateCastMemberUseCase } from "#cast-member/application";
+import { Types } from "#cast-member/domain";
 import { CastMemberSequelize } from "#cast-member/infra";
+import { EntityValidationError } from "#seedwork/domain";
 import { setupSequelize } from "#seedwork/infra/testing/helpers/db";
 
 const { CastMemberModel, CastMemberRepository } = CastMemberSequelize;
@@ -15,17 +17,44 @@ describe("CastMemberSequelizeRepository Integration Tests", () => {
     useCase = new CreateCastMemberUseCase.UseCase(repository);
   });
 
+  it("should throw a generic error", async () => {
+    // Arrange
+    const genericError = new Error("Generic Error");
+    jest.spyOn(repository, "insert").mockRejectedValue(genericError);
+    // Act & Assert
+    await expect(
+      useCase.execute({ name: "John Doe", type: Types.ACTOR })
+    ).rejects.toThrowError(genericError);
+  });
+
+  it("should throw an entity validation error", async () => {
+    try {
+      await useCase.execute({} as any);
+      fail("should throw an entity validation error");
+    } catch (e) {
+      expect(e).toBeInstanceOf(EntityValidationError);
+      expect(e.error).toEqual({
+        name: [
+          "name should not be empty",
+          "name must be a string",
+          "name must be shorter than or equal to 255 characters",
+        ],
+        type: ["Invalid cast member type: undefined"],
+      });
+    }
+  });
+
   describe("should create cast member", () => {
     const arrange = [
       {
-        inputProps: { name: "John Doe", type: 1 },
+        inputProps: { name: "John Doe", type: 1 as Types },
         outputProps: {
           name: "John Doe",
           type: 1,
         },
       },
       {
-        inputProps: { name: "John Doe", type: 2 },
+        inputProps: { name: "John Doe", type: 2 as Types },
         outputProps: {
           name: "John Doe",
           type: 2,
