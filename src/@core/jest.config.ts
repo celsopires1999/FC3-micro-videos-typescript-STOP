5cf642168f06398723f696a0408906e3da614bbd
celsopires1999 <celso.pires1999@gmail.com>
2023-03-08 00:22:53 +0000

    test: Remove index.ts from test coverage

--- src/@core/jest.config.ts
+++ src/@core/jest.config.ts
@@ -30,9 +30,7 @@ export default {
   coverageDirectory: "../__coverage",
 
   // An array of regexp pattern strings used to skip coverage collection
-  // coveragePathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  coveragePathIgnorePatterns: ["index.ts"],
 
   // Indicates which provider should be used to instrument code for coverage
   coverageProvider: "v8",
