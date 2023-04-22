3755da1f88793d0797f71b32dcab878b0f14ea82
celsopires1999 <celso.pires1999@gmail.com>
2023-04-22 00:09:01 +0000

    feat: Novo padrão Either

--- src/@core/src/@seedwork/domain/utils/either.spec.ts
+++ src/@core/src/@seedwork/domain/utils/either.spec.ts
@@ -1,51 +1,124 @@
 import { Either } from "./either";
 
 describe("Either Unit Tests", () => {
-  it("should be an Array instance", () => {
-    const either = new Either(1, 2);
-    expect(either).toBeInstanceOf(Array);
-  });
+  test("of", () => {
+    const either = Either.of<number>(1);
+    expect(either.isOk()).toBeTruthy();
+    expect(either.isFail()).toBeFalsy();
+    expect(either.ok).toBe(1);
+    expect(either.error).toBeNull();
 
-  test("constructor", () => {
-    const either = new Either(1, 2);
-    expect(either[0]).toBe(1);
-    expect(either[1]).toBe(2);
+    const [ok, error] = either.asArray();
+    expect(ok).toBe(1);
+    expect(error).toBeNull();
   });
 
   test("ok", () => {
-    const either = Either.ok(1);
-    expect(either[0]).toBe(1);
-    expect(either[1]).toBeNull();
+    const either = Either.ok<number>(1);
+    expect(either.isOk()).toBeTruthy();
+    expect(either.isFail()).toBeFalsy();
+    expect(either.ok).toBe(1);
+    expect(either.error).toBeNull();
+
+    const [ok, error] = either.asArray();
+    expect(ok).toBe(1);
+    expect(error).toBeNull();
   });
 
   test("fail", () => {
-    const error = new Error("Oops!");
-    const either = Either.fail(error);
-    expect(either[0]).toBeNull();
-    expect(either[1]).toEqual(error);
-  });
-
-  test("getOK", () => {
-    const either = Either.ok("Success");
-    expect(either.getOk()).toBe("Success");
-  });
-
-  test("getFail", () => {
-    const error = new Error("Oops!");
-    const either = Either.fail(error);
-    expect(either.getFail()).toEqual(error);
-  });
-
-  test("isOK", () => {
-    const either = Either.ok("Success");
-    expect(either.isOk()).toBeTruthy();
-    expect(either.isFail()).toBeFalsy();
-  });
-
-  test("getFail", () => {
-    const error = new Error("Oops!");
-    const either = Either.fail(error);
-    expect(either.isFail()).toBeTruthy();
+    const oops = new Error("Oops!");
+    const either = Either.fail(oops);
     expect(either.isOk()).toBeFalsy();
+    expect(either.isFail()).toBeTruthy();
+    expect(either.ok).toBeNull();
+    expect(either.error).toEqual(oops);
+
+    const [ok, error] = either.asArray();
+    expect(ok).toBeNull();
+    expect(error).toEqual(oops);
+  });
+
+  test("safe", () => {
+    const either1 = Either.safe(() => 1);
+    expect(either1.isOk()).toBeTruthy();
+    expect(either1.isFail()).toBeFalsy();
+    expect(either1.ok).toBe(1);
+    expect(either1.error).toBeNull();
+
+    const oops = new Error("Oops!");
+    const either2 = Either.safe(() => {
+      throw oops;
+    });
+    expect(either2.isOk()).toBeFalsy();
+    expect(either2.isFail()).toBeTruthy();
+    expect(either2.ok).toBeNull();
+    expect(either2.error).toEqual(oops);
+
+    const [ok, error] = either2.asArray();
+    expect(ok).toBeNull();
+    expect(error).toEqual(oops);
+  });
+
+  test("map method", () => {
+    const either1 = Either.ok(1);
+    const newEither = either1
+      .map((value) => value + 1)
+      .map((value) => value + 1);
+    expect(newEither.isOk()).toBeTruthy();
+    expect(newEither.isFail()).toBeFalsy();
+    expect(newEither.ok).toBe(3);
+    expect(newEither.error).toBeNull();
+
+    const either2 = Either.fail(1);
+    //@ts-expect-error: this is a test
+    const newEither2 = either2.map((value) => value + 1);
+    expect(newEither2.isOk()).toBeFalsy();
+    expect(newEither2.isFail()).toBeTruthy();
+    expect(newEither2.ok).toBeNull();
+    expect(newEither2.error).toBe(1);
+  });
+
+  test("chain method", () => {
+    const either1 = Either.ok(1);
+    const newEither = either1
+      .chain((value) => Either.ok(value + 1))
+      .chain((value) => Either.ok(value + 1));
+    expect(newEither.isOk()).toBeTruthy();
+    expect(newEither.isFail()).toBeFalsy();
+    expect(newEither.ok).toBe(3);
+    expect(newEither.error).toBeNull();
+
+    const either2 = Either.fail(1);
+    //@ts-expect-error: this is a test
+    const newEither2 = either2.chain((value) => Either.ok(value + 1));
+    expect(newEither2.isOk()).toBeFalsy();
+    expect(newEither2.isFail()).toBeTruthy();
+    expect(newEither2.ok).toBeNull();
+    expect(newEither2.error).toBe(1);
+  });
+
+  test("chainEach method", () => {
+    const either1 = Either.ok(1);
+    //@ts-expect-error: this is a test
+    expect(() => either1.chainEach((value) => value)).toThrowError(
+      "Method chainEach only works with arrays"
+    );
+
+    const either2 = Either.ok([1, 2, 3]);
+    const newEither2 = either2
+      .chainEach((value) => Either.ok(value + 1))
+      .chainEach((value) => Either.ok(value + 1));
+    expect(newEither2.isOk()).toBeTruthy();
+    expect(newEither2.isFail()).toBeFalsy();
+    expect(newEither2.ok).toEqual([3, 4, 5]);
+    expect(newEither2.error).toBeNull();
+
+    const either3 = Either.fail(1);
+    //@ts-expect-error: this is a test
+    const newEither3 = either3.chainEach((value) => Either.ok(value + 1));
+    expect(newEither3.isOk()).toBeFalsy();
+    expect(newEither3.isFail()).toBeTruthy();
+    expect(newEither3.ok).toBeNull();
+    expect(newEither3.error).toBe(1);
   });
 });
--- src/@core/src/@seedwork/domain/utils/either.ts
+++ src/@core/src/@seedwork/domain/utils/either.ts
@@ -1,8 +1,40 @@
-const EitherConstructor: new <Ok, Error>(...p: [Ok, Error]) => [Ok, Error] =
-  Array as any;
-export class Either<Ok, Error = any> extends EitherConstructor<Ok, Error> {
-  constructor(...p: [Ok, Error]) {
-    super(...p);
+// O either será um array personalizado do JavaScript
+// O 1º elemento é o valor
+// o 2º elemento é o erro
+
+type Flatten<Type> = Type extends Array<infer Item> ? Item : Type;
+
+type IteratorValue<Ok, Error> = Ok | Error;
+
+export class Either<Ok, Error = any>
+  implements Iterable<IteratorValue<Ok, Error>>
+{
+  private _ok: Ok;
+  private _error: Error;
+
+  private constructor(ok: Ok, error: Error) {
+    this._ok = ok;
+    this._error = error;
+  }
+
+  get ok() {
+    return this._ok;
+  }
+
+  get error() {
+    return this._error;
+  }
+
+  isOk() {
+    return this.ok !== null;
+  }
+
+  isFail() {
+    return this.error !== null;
+  }
+
+  static of<Ok = any>(value: Ok): Either<Ok> {
+    return Either.ok(value);
   }
 
   static ok<T>(value: T): Either<T, null> {
@@ -13,19 +45,95 @@ export class Either<Ok, Error = any> extends EitherConstructor<Ok, Error> {
     return new Either(null, error);
   }
 
-  getOk() {
-    return this[0];
+  static safe<Ok, Error = any>(fn: () => Ok): Either<Ok, Error> {
+    try {
+      return Either.ok(fn());
+    } catch (e) {
+      return Either.fail(e);
+    }
   }
 
-  getFail() {
-    return this[1];
+  map<NewOk>(fn: (value: Ok) => NewOk): Either<NewOk, Error> {
+    if (this.isOk()) {
+      return Either.ok(fn(this.ok));
+    }
+    return Either.fail(this.error);
   }
 
-  isOk() {
-    return this[1] === null;
+  chain<NewOk, NewError = any>(
+    fn: (value: Ok) => Either<NewOk, NewError>
+  ): Either<NewOk, Error | NewError> {
+    if (this.isOk()) {
+      return fn(this.ok);
+    }
+    return Either.fail(this.error);
   }
 
-  isFail() {
-    return this[1] !== null;
+  chainEach<NewOk, NewError = any>(
+    fn: (value: Flatten<Ok>) => Either<Flatten<NewOk>, Flatten<NewError>>
+  ): Either<NewOk, Error | NewError> {
+    if (this.isOk()) {
+      if (!Array.isArray(this.ok)) {
+        throw new Error("Method chainEach only works with arrays");
+      }
+      const result = this.ok.map((o) => {
+        return fn(o);
+      });
+      const errors = result.filter((r) => r.isFail());
+      if (errors.length > 0) {
+        return Either.fail(errors.map((e) => e.error)) as any;
+      }
+      return Either.ok(result.map((r) => r.ok)) as any;
+    }
+    return Either.fail<Error>(this.error);
+  }
+
+  asArray(): [Ok, Error] {
+    return [this.ok, this.error];
+  }
+
+  [Symbol.iterator](): Iterator<IteratorValue<Ok, Error>, any, undefined> {
+    return new EitherIterator<Ok, Error>({
+      ok: this.ok,
+      error: this.error,
+    });
+  }
+}
+
+class EitherIterator<Ok, Error>
+  implements
+    Iterator<IteratorValue<Ok, Error>, IteratorValue<Ok, Error>, undefined>
+{
+  private _value: { ok: Ok; error: Error };
+  private index = 0;
+
+  constructor(value: { ok: Ok; error: Error }) {
+    this._value = value;
+  }
+
+  next(): IteratorResult<
+    IteratorValue<Ok, Error>,
+    IteratorValue<Ok, Error> | undefined
+  > {
+    if (this.index === 0) {
+      this.index++;
+      return {
+        value: this._value.ok,
+        done: false,
+      };
+    }
+
+    if (this.index === 1) {
+      this.index++;
+      return {
+        value: this._value.error,
+        done: false,
+      };
+    }
+
+    return {
+      value: null,
+      done: true,
+    };
   }
 }
